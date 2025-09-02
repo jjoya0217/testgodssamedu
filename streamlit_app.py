e06472e8d498be4037364e1be5d8c14b491a2014
jjoya0217 <mathteacherhwang@gmail.com>
2025-09-02 04:44:09 +0000

    v01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# 기존 코드: st.title("My First App")
+# 수정 코드:
+st.title("🏫 화신중 알림장")
+st.write("화신중학교의 중요한 소식을 확인하세요!")
